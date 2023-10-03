b3108a7dc5286e99bdc6d4c302ad5f02a4418d4a
NGUYEN LUAN <nguyenluan0101020@gmail.com>
2023-10-03 15:30:54 +0700

    update API

---
+++ src/main/java/com/nd2/assignwork/api/DocumentIncommingAPI.java
@@ -0,0 +1,10 @@
+package com.nd2.assignwork.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class DocumentIncommingAPI {
+
+}
